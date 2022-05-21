d171eaf7a55b459cf7b5a58d0403ba500aea227a
WellingtonBP <wellingtonpacheco14@gmail.com>
2022-05-21 14:20:21 -0300

    add: user schema

---
+++ priv/repo/migrations/20220521042257_create_users_table.exs
@@ -0,0 +1,18 @@
+defmodule ExgamesServer.Repo.Migrations.CreateUsersTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:users, primary_key: false) do
+      add :id, :uuid, primary_key: true
+      add :name, :string, null: false
+      add :email, :string, null: false
+      add :avatar_url, :string
+      add :birthday, :date
+      add :hashed_password, :string
+
+      timestamps()
+    end
+
+    create unique_index(:users, [:name, :email])
+  end
+end
